ca47c100138303c4be1f49c98acffe33c3a083fc
naelfox <natanaelvila2@gmail.com>
2023-05-01 11:21:49 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="Banner">
+            <img src="/imagens/banner.png" alt="banner principal"/>
+        </header>
+    );
+}
+
+export default Banner;
